4a6ddfde415c0b06eaac095328ebcad3cd2d41de
pat <pat_zhuang@outlook.com>
2021-12-24 11:46:42 +0800

    暂时屏蔽 maxpooling, 保留 mp end

--- r2r_src/model_PREVALENT.py
+++ r2r_src/model_PREVALENT.py
@@ -106,8 +106,8 @@ class VLNBERT(nn.Module):
             state_with_action = self.action_LayerNorm(state_with_action)
             state_feats = torch.cat((state_with_action.unsqueeze(1), sentence[:, 1:, :]), dim=1)
 
-            if cand_mp_feats is not None:
-                cand_feats[..., :-args.angle_feat_size] += self.feat_cat_alpha * cand_mp_feats
+            # if cand_mp_feats is not None:
+            #     cand_feats[..., :-args.angle_feat_size] += self.feat_cat_alpha * cand_mp_feats
             # cand_feats[..., :-args.angle_feat_size] = self.drop_env(cand_feats[..., :-args.angle_feat_size])
             # logit is the attention scores over the candidate features
             h_t, logit, attended_language, attended_visual, language_attn_probs = self.vln_bert(mode, state_feats,
