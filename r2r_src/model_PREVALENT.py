aa942f6a922d0dfe297d8bce151fa94ae88281ff
pat <pat_zhuang@outlook.com>
2021-12-30 16:05:54 +0800

    dropenv on slot attention

--- r2r_src/model_PREVALENT.py
+++ r2r_src/model_PREVALENT.py
@@ -110,6 +110,8 @@ class VLNBERT(nn.Module):
             #     cand_feats[..., :-args.angle_feat_size] += self.feat_cat_alpha * cand_mp_feats
             if not args.slot_attn:
                 cand_feats[..., :-args.angle_feat_size] = self.drop_env(cand_feats[..., :-args.angle_feat_size])
+            else:
+                cand_feats = self.drop_env(cand_feats)
             # logit is the attention scores over the candidate features
             h_t, logit, attended_language, attended_visual, language_attn_probs = self.vln_bert(mode, state_feats,
                                                                            attention_mask=attention_mask,
